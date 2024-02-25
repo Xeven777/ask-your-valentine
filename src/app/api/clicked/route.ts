670d4d1567895b948c645568a766e1b95b2ebdb8
Anish <xevenbiswas@gmail.com>
2024-02-25 21:57:42 +0530

    check

--- src/app/api/clicked/route.ts
+++ src/app/api/clicked/route.ts
@@ -2,23 +2,28 @@ import { generateEmailBody } from "@/actions";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextApiRequest, res: NextResponse) {
   // let ip = req.headers["x-real-ip"] as string;
   // const forwardedFor = req.headers["x-forwarded-for"] as string;
   // if (!ip && forwardedFor) {
   //   ip = forwardedFor?.split(",").at(0) ?? "Unknown";
   // }
   try {
-    const userData = {
-      ip: req.ip,
-      region: req.geo?.region,
-      city: req.geo?.city,
-      country: req.geo?.country,
-      latitude: req.geo?.latitude,
-      longitude: req.geo?.longitude,
-    };
-    const emailbody = await generateEmailBody(userData);
-    return NextResponse.json({ success: true, userData });
+    let ip = req.headers["x-real-ip"] as string;
+    const forwardedFor = req.headers["x-forwarded-for"] as string;
+    if (!ip && forwardedFor) {
+      ip = forwardedFor?.split(",").at(0) ?? "Unknown";
+    }
+    // const userData = {
+    //   ip: req.ip,
+    //   region: req.geo?.region,
+    //   city: req.geo?.city,
+    //   country: req.geo?.country,
+    //   latitude: req.geo?.latitude,
+    //   longitude: req.geo?.longitude,
+    // };
+    // const emailbody = await generateEmailBody(userData);
+    return NextResponse.json({ success: true, ip });
   } catch (error) {
     throw new Error("Failed");
   }
