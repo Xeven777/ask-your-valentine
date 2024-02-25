94dec23a143125ce9c0ed464bbec00cf1ae1831d
Anish <xevenbiswas@gmail.com>
2024-02-25 22:56:21 +0530

    chnge ua

--- src/app/api/click/route.ts
+++ src/app/api/click/route.ts
@@ -4,15 +4,21 @@ import { NextRequest, NextResponse, userAgent } from "next/server";
 import { headers } from "next/headers";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const { ua } = userAgent(req);
+  const useragent = userAgent(req);
 
   const headersList = headers();
-  const ip = headersList.get("x-forwarded-for");
+  const ip =
+    headersList.get("x-forwarded-for") ||
+    headersList.get("x-real-ip") ||
+    headersList.get("x-client-ip") ||
+    headersList.get("x-remote-ip") ||
+    headersList.get("x-remote-addr") ||
+    "couldnt find ip address";
 
   const data = {
     ok: true,
     ip_address: ip,
-    user_agent: ua,
+    user_agent: useragent,
   };
   // const emailbody = await generateEmailBody(userData);
   return NextResponse.json({ success: true, data });
