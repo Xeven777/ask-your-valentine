abc1abaa7a6b52dc620005924f63a4c785f1b490
Anish <xevenbiswas@gmail.com>
2024-02-26 19:05:24 +0530

    nodemailer changes

--- src/actions.ts
+++ src/actions.ts
@@ -56,11 +56,28 @@ export const sendEmail = async (emailContent: EmailContent, sendTo: string) => {
     html: emailContent.body,
     subject: emailContent.subject,
   };
-
-  transporter.sendMail(mailOptions, (error: any, info: any) => {
-    if (error) return console.log(error);
-
-    console.log("Email sent from server ! ", info);
+  await new Promise((resolve, reject) => {
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
   });
 };
 
