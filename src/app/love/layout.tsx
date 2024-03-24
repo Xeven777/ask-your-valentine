b479224b17f886db595b86ae0b4bdb9a1cddb2e0
Anish <xevenbiswas@gmail.com>
2024-03-25 00:44:11 +0530

    fixed url and client compnents

--- src/app/love/layout.tsx
+++ src/app/love/layout.tsx
@@ -2,17 +2,17 @@ import { simpleDecrypt } from "@/actions";
 import { Metadata } from "next";
 import { headers } from "next/headers";
 
-export async function generateMetadata(): Promise<Metadata> {
-  const headersList = headers();
-  const fullUrl = headersList.get("referer") || "";
-  const urlParams = new URLSearchParams(new URL(fullUrl).search);
-  const n = await simpleDecrypt(urlParams.get("n") || "");
+// export async function generateMetadata(): Promise<Metadata> {
+//   const headersList = headers();
+//   const fullUrl = headersList.get("referer") || "";
+//   const urlParams = new URLSearchParams(new URL(fullUrl).search);
+//   const n = await simpleDecrypt(urlParams.get("n") || "");
 
-  return {
-    title: "Hey " + n || "Cutie",
-    description: "I want to ask you something",
-  };
-}
+//   return {
+//     title: "Hey " + n || "Cutie",
+//     description: "I want to ask you something",
+//   };
+// }
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
--- src/app/love/page.tsx
+++ src/app/love/page.tsx
@@ -1,140 +1,33 @@
-/* eslint-disable @next/next/no-img-element */
-"use client";
-import { useSearchParams } from "next/navigation";
-import { toast } from "sonner";
-import { useEffect, useState } from "react";
-import lovesvg from "../../assets/Love In The Air SVG Cut File.svg";
-import lovesvg2 from "../../assets/All You Need Is Love SVG Cut File.svg";
-import Image from "next/image";
-import { Footer } from "@/components/Footer";
+import MainBox from "@/components/MainBox";
+import { Metadata } from "next";
+interface SearchParams {
+  q: string;
+  n: string;
+  e: string;
+}
 
-export default function Page() {
-  const searchParams = useSearchParams();
-  const [noCount, setNoCount] = useState(0);
-  const [yesPressed, setYesPressed] = useState(false);
-  const yesButtonSize = noCount * 20 + 16;
-  const [name, setName] = useState("Cutie");
-  const [question, setQuestion] = useState("Will you be my valentine?");
-  useEffect(() => {
-    setName(
-      simpleDecrypt(decodeURIComponent(searchParams.get("n") || "Cutie"))
-    );
-    setQuestion(
-      simpleDecrypt(
-        decodeURIComponent(searchParams.get("q") || "Will you be my valentine?")
-      )
-    );
-  }, [searchParams]);
-  const handleNoClick = () => {
-    setNoCount(noCount + 1);
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}): Promise<Metadata> {
+  const name = simpleDecrypt(decodeURIComponent(searchParams.n || "Cutie"));
+
+  return {
+    title: "Hey " + name,
+    description: "I want to ask you something",
   };
+}
 
-  const handleYesClick = async () => {
-    setYesPressed(true);
-
-    const email = decodeURIComponent(searchParams.get("e") || "");
-
-    try {
-      const fetchPromise = fetch("/api/click", {
-        method: "POST",
-        body: JSON.stringify({ email: email }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      toast.promise(fetchPromise, {
-        loading: "Loading... We are sending your response! 💌",
-        success: "Wohoo! Message sent! 🥰",
-        error: "Oops,Error! Try again!",
-      });
-    } catch (error) {
-      console.error("Fetch error:", error);
-      toast("Error sending response! Please try again later! 😢");
-    }
-  };
-  const getNoButtonText = () => {
-    const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Plsss? :( You're breaking my heart",
-    ];
-
-    return phrases[Math.min(noCount, phrases.length - 1)];
-  };
-
+export default function Page({ searchParams }: { searchParams: SearchParams }) {
   return (
     <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen -mt-16 selection:bg-rose-600 selection:text-white text-zinc-900">
-      {yesPressed ? (
-        <>
-          <img
-            src="https://gifdb.com/images/high/yay-milk-and-mocha-bears-cheering-confetti-9rjvz35rjxvj7oup.gif"
-            alt="Yaaayyy"
-          />
-          <div className="text-4xl md:text-6xl font-bold my-4 riot">
-            Ok Yayyyyy!!!
-          </div>
-        </>
-      ) : (
-        <>
-          <Image
-            src={lovesvg}
-            alt="love"
-            className="fixed animate-pulse top-10 md:left-24 left-6 md:w-40 w-28"
-          />
-          <Image
-            alt="love"
-            src={lovesvg2}
-            className="fixed bottom-16 -z-10 animate-pulse md:right-24 right-10 md:w-40 w-32"
-          />
-          <img
-            alt="love"
-            className="h-[230px] rounded-lg shadow-lg"
-            src="https://gifdb.com/images/high/cute-Love-bear-roses-ou7zho5oosxnpo6k.gif"
-          />
-          <h1 className="text-4xl md:text-6xl my-4 text-center riot max-w-4xl">
-            {name}, {question}
-          </h1>
-          <div className="flex flex-wrap justify-center gap-2 items-center">
-            <button
-              type="button"
-              className={`bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mr-4`}
-              style={{ fontSize: yesButtonSize }}
-              onClick={handleYesClick}
-            >
-              Yes
-            </button>
-            <button
-              onClick={handleNoClick}
-              className=" bg-rose-500 hover:bg-rose-600 rounded-lg text-white font-bold py-2 px-4"
-            >
-              {noCount === 0 ? "No" : getNoButtonText()}
-            </button>
-          </div>
-          <Footer />
-        </>
-      )}
+      <MainBox searchParams={searchParams} />
     </div>
   );
 }
-
 function simpleDecrypt(text: string): string {
-  const key = process.env.NEXT_PUBLIC_KEY || "abcd";
+  const key = process.env.NEXT_PUBLIC_KEY || "abcdefghijlkmnopqrstuvwxyz";
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
   const decryptedText = text
     .split("")
